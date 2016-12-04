14775b7d6028890ec42e5931d3b7d1ca7537b3eb
tammyyu <tammyyu@uw.edu>
2016-12-04 13:41:26 -0800

    Fixed formatting

--- src/components/faq.jsx
+++ src/components/faq.jsx
@@ -18,19 +18,20 @@ export default class extends React.Component {
             <div className="about">
             
                 <p>FAQ</p>
+                
                 <div className="container">
                     <ul className="collapsible" data-collapsible="accordion">
                         <li>
-                        <div className="collapsible-header"><i className="material-icons">filter_drama</i>First</div>
-                        <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                            <div className="collapsible-header"><i className="material-icons">filter_drama</i>First</div>
+                            <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
                         </li>
                         <li>
-                        <div className="collapsible-header"><i className="material-icons">place</i>Second</div>
-                        <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                            <div className="collapsible-header"><i className="material-icons">place</i>Second</div>
+                            <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
                         </li>
                         <li>
-                        <div className="collapsible-header"><i className="material-icons">whatshot</i>Third</div>
-                        <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                            <div className="collapsible-header"><i className="material-icons">whatshot</i>Third</div>
+                            <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
                         </li>
                     </ul>
                 </div>
