da316bdedabafcc7c4ce711cb56f73c2053fbf93
tammyyu <tammyyu@uw.edu>
2016-12-07 19:46:21 -0800

    Fixed indentation in main

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -8,13 +8,9 @@ export default class extends React.Component {
     }
     
     componentDidMount() {
+        $('.carousel').carousel();
         $('.parallax').parallax();
     }
-
-        componentDidMount() {
-            $('.carousel').carousel();
-            $('.parallax').parallax();
-        }
 
     render() {
         var img_calm = "../img/products/calm-transparent.png";
@@ -27,8 +23,8 @@ export default class extends React.Component {
             <AwardCard key={award.name} className="awardCardMain" award={award}></AwardCard>
         ));
         return (
-        <div className="main">
-            <div>
+            <div className="main">
+
                 <div id="myCarousel" className="carousel slide" data-ride="carousel"> 
                     <ol className="carousel-indicators">
                         <li data-target="#myCarousel" data-slide-to={0} className="active" />
@@ -59,91 +55,91 @@ export default class extends React.Component {
                 </div>
 
                 <div className="bestSellers">
-                <h2>Best-Sellers</h2>
+                    <h2>Best-Sellers</h2>
 
-                <div className="container">
-                
-                    <div className="products col s12 m7">
-                        <div className="card horizontal">
-                            <div className="card-image">
-                                <img className="product-image" src={img_calm} height={img_height}/>
-                            </div>
-                            <div className="card-stacked products-text">
-                                <h5>Calm</h5>
-                                <i>For tranquil, relaxed behavior</i>
-                                <div className="card-content">
-                                    <p>Calms a stressed dog or cat. It also relaxes hyper animals, such as puppies and kittens. Use it to encourage restful, all night sleep.</p>
+                    <div className="container">
+                        <div className="products col s12 m7">
+                            <div className="card horizontal">
+                                <div className="card-image">
+                                    <img className="product-image" src={img_calm} height={img_height}/>
                                 </div>
-                                <div className="card-action">
-                                    <a href="#">This is a link</a>
+                                <div className="card-stacked products-text">
+                                    <h5>Calm</h5>
+                                    <i>For tranquil, relaxed behavior</i>
+                                    <div className="card-content">
+                                        <p>Calms a stressed dog or cat. It also relaxes hyper animals, such as puppies and kittens. Use it to encourage restful, all night sleep.</p>
+                                    </div>
+                                    <div className="card-action">
+                                        <a href="#">This is a link</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="products col s12 m7">
-                        <div className="card horizontal">
-                            <div className="card-image">
-                                <img className="product-image" src={img_skin} height={img_height}/>
-                            </div>
-                            <div className="card-stacked products-text">
-                                <h5>Skin</h5>
-                                <i>Relieves itchy, flaky skin</i>
-                                <div className="card-content">
-                                    <p>Promotes healthy skin and coats. It also relieves scratchy, dry skin. Helps ease discomfort from flea bites and other skin irritants.</p>
+                        <div className="products col s12 m7">
+                            <div className="card horizontal">
+                                <div className="card-image">
+                                    <img className="product-image" src={img_skin} height={img_height}/>
                                 </div>
-                                <div className="card-action">
-                                    <a href="#">This is a link</a>
+                                <div className="card-stacked products-text">
+                                    <h5>Skin</h5>
+                                    <i>Relieves itchy, flaky skin</i>
+                                    <div className="card-content">
+                                        <p>Promotes healthy skin and coats. It also relieves scratchy, dry skin. Helps ease discomfort from flea bites and other skin irritants.</p>
+                                    </div>
+                                    <div className="card-action">
+                                        <a href="#">This is a link</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="products col s12 m7">
-                        <div className="card horizontal">
-                            <div className="card-image">
-                                <img className="product-image" src={img_happy} height={img_height}/>
-                            </div>
-                            <div className="card-stacked products-text">
-                                <h5>Happy</h5>
-                                <i>Reduces stress and anxiety</i>
-                                <div className="card-content">
-                                    <p>Reduces stress from grief, separation anxiety, and environmental changes. It helps your pet feel better and get back to normal.</p>
+                        <div className="products col s12 m7">
+                            <div className="card horizontal">
+                                <div className="card-image">
+                                    <img className="product-image" src={img_happy} height={img_height}/>
                                 </div>
-                                <div className="card-action">
-                                    <a href="#">This is a link</a>
+                                <div className="card-stacked products-text">
+                                    <h5>Happy</h5>
+                                    <i>Reduces stress and anxiety</i>
+                                    <div className="card-content">
+                                        <p>Reduces stress from grief, separation anxiety, and environmental changes. It helps your pet feel better and get back to normal.</p>
+                                    </div>
+                                    <div className="card-action">
+                                        <a href="#">This is a link</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="products col s12 m7">
-                        <div className="card horizontal">
-                            <div className="card-image">
-                                <img className="product-image" src={img_repair} height={img_height}/>
-                            </div>
-                            <div className="card-stacked products-text">
-                                <h5>Repair</h5>
-                                <i>For faster injury recovery</i>
-                                <div className="card-content">
-                                    <p>Helps boost the body’s immune response and relieves aches, pains and inflammation. It is good for injury recovery and supports the body during illness.</p>
+                        <div className="products col s12 m7">
+                            <div className="card horizontal">
+                                <div className="card-image">
+                                    <img className="product-image" src={img_repair} height={img_height}/>
                                 </div>
-                                <div className="card-action">
-                                    <a href="#">This is a link</a>
+                                <div className="card-stacked products-text">
+                                    <h5>Repair</h5>
+                                    <i>For faster injury recovery</i>
+                                    <div className="card-content">
+                                        <p>Helps boost the body’s immune response and relieves aches, pains and inflammation. It is good for injury recovery and supports the body during illness.</p>
+                                    </div>
+                                    <div className="card-action">
+                                        <a href="#">This is a link</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
                     </div>
 
                 </div> 
-                <section className="awards">
-                        <h2>Awards</h2>
-                        <div className="award-list-main">                
-                            {awardCard}
-                        </div>
-                </section>
+
+                <div className="awards">
+                    <h2>Awards</h2>
+                    <div className="award-list-main">                
+                        {awardCard}
+                    </div>
                 </div>
+
             </div>
         );
     }
