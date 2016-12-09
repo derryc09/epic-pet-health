46204e32b3e0a2da6310780b7940855982534f81
Andrew Yu <derryc09@uw.edu>
2016-12-08 16:52:00 -0800

    fixed form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -28,40 +28,43 @@ export default class extends React.Component {
 
              <div className="row contact-form">
                     <h2 className="centerTitle">Contact Us</h2>
-                    <form className="col s12" method="post" action="mailto:derryc0907@gmail.com" >
+                    <form className="col s12" action="https://formspree.io/derryc0907@gmail.com"
+                    method="POST" >
                     <div className="row">
                         <div className="input-field col s6">
                         <i className="material-icons prefix">account_circle</i>
-                        <input id="icon_prefix" type="text" className="validate"/>
+                        <input id="icon_prefix" type="text" className="validate" name="FirstName"/>
                         <label className = "active" htmlFor="icon_prefix">First Name</label>
                         </div>
                         <div className="input-field col s6">
                         <i className="material-icons prefix">account_circle</i>
-                        <input id="icon_prefix" type="text" className="validate"/>
+                        <input id="icon_prefix" type="text" className="validate" name="LastName"/>
                         <label className = "active" htmlFor="icon_prefix">Last Name</label>
                         </div>                        
                         <div className="input-field col s6">
                         <i className="material-icons prefix">phone</i>
-                        <input id="icon_telephone" type="tel" className="validate"/>
+                        <input id="icon_telephone" type="tel" className="validate" name="phone"/>
                         <label className = "active" htmlFor="icon_telephone">Telephone</label>
                         </div>
 
                         <div className="input-field col s6">
                         <i className="material-icons prefix">email</i>
-                        <input id="icon_email" type="tel" className="validate"/>
+                        <input id="icon_email" type="tel" className="validate" name="_replyto"/>
                         <label className = "active" htmlFor="icon_email">Email</label>
                         </div>  
 
                         <div className="row">
-                        <div className="input-field col s12">
-                            <i className="material-icons prefix">comment</i>
-                        
-                            <textarea id="textarea1" className="materialize-textarea" ></textarea>
-                            <label className="active" htmlFor="textarea1">Comments</label>
+                            <div className="input-field col s12">
+                                <i className="material-icons prefix">comment</i>
+                            
+                                <textarea id="textarea1" className="materialize-textarea" name="Message"></textarea>
+                                <label className="active" htmlFor="textarea1">Comments</label>
+                            </div>
                         </div>
-                        </div>      
-                        <div className = "submitButton" type="submit" value="Send Email" onClick={this.submitEmail}>                  
-                            <a className="waves-effect waves-light btn"><i className="material-icons left">cloud</i>submit</a>
+      
+                        <div className = "submitButton waves-effect waves-light btn" onClick={this.submitEmail}>    
+                            <input type="submit" value="Send"/>
+                                <i className="material-icons left">cloud</i>
                         </div>
                     </div>
                     </form>
