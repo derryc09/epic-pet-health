4bf5069e4df252b3c10005848d6b9452f7d270e0
Andrew Yu <derryc09@uw.edu>
2016-12-11 18:46:50 -0800

    change paths

--- data/products.js
+++ data/products.js
@@ -1,6 +1,6 @@
 var PRODUCTS = [{
     "name": "Align",
-    "img": "../img/products/align.jpg",
+    "img": "img/products/align.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/align",
     "bestseller": false,
     "agility": true,
@@ -12,7 +12,7 @@ var PRODUCTS = [{
     "description": "Relaxes your pet’s spinal nerves and muscles. It reduces pain and simulates a chiropractic adjustment."
 }, {
     "name": "Balance",
-    "img": "../img/products/balance.jpg",
+    "img": "img/products/balance.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/balance",
     "bestseller": false,
     "agility": true,
@@ -24,7 +24,7 @@ var PRODUCTS = [{
     "description": "Improves balance and stability in older or sick pets. It can be used in conjunction with a variety of other supplements."
 }, {
     "name": "Bones",
-    "img": "../img/products/bones.jpg",
+    "img": "img/products/bones.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/bones",
     "bestseller": false,
     "agility": false,
@@ -36,7 +36,7 @@ var PRODUCTS = [{
     "description": "Supports healthy bones. Use to improve or maintain healthy bone density in elderly pets."
 }, {
     "name": "Calm",
-    "img": "../img/products/calm.jpg",
+    "img": "img/products/calm.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/calm",
     "bestseller": true,
     "agility": false,
@@ -48,7 +48,7 @@ var PRODUCTS = [{
     "description": "Improves your pet’s cognition and mental processes. It is great for elderly pets."
 }, {
     "name": "Cat Digest",
-    "img": "../img/products/cat-digest.jpg",
+    "img": "img/products/cat-digest.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/digest",
     "bestseller": true,
     "agility": false,
@@ -60,7 +60,7 @@ var PRODUCTS = [{
     "description": "Decreases hairballs."
 }, {
     "name": "Clarity",
-    "img": "../img/products/clarity.jpg",
+    "img": "img/products/clarity.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/clarity",
     "bestseller": false,
     "agility": false,
@@ -72,7 +72,7 @@ var PRODUCTS = [{
     "description": "Improves your pet’s cognition and mental processes. It is great for elderly pets."
 }, {
     "name": "Clean Teeth",
-    "img": "../img/products/clean-teeth.jpg",
+    "img": "img/products/clean-teeth.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/clean-teeth",
     "bestseller": true,
     "agility": true,
@@ -84,7 +84,7 @@ var PRODUCTS = [{
     "description": "Reduces your pet’s dental tartar and bad breath. It also promotes healthy teeth and gums."
 }, {
     "name": "Clear",
-    "img": "../img/products/clear.jpg",
+    "img": "img/products/clear.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/clear",
     "bestseller": true,
     "agility": false,
@@ -96,7 +96,7 @@ var PRODUCTS = [{
     "description": "Reduces allergy symptoms in dogs and cats. It also treats and relieves scratching, sneezing and coughing as well."
 }, {
     "name": "Digest",
-    "img": "../img/products/digest.jpg",
+    "img": "img/products/digest.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/digest-1",
     "bestseller": false,
     "agility": false,
@@ -108,7 +108,7 @@ var PRODUCTS = [{
     "description": "Natural treatment for dogs and cats with gas pain. It reduces bad breath and promotes normal, healthy digestion. Help your pets gain better digestive health!"
 }, {
     "name": "Eye",
-    "img": "../img/products/eye.jpg",
+    "img": "img/products/eye.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/eye",
     "bestseller": false,
     "agility": false,
@@ -120,7 +120,7 @@ var PRODUCTS = [{
     "description": "All natural eye drop for dogs and cats with sensitive eyes. This supplement improves itchy eyes, runny eyes, and brown discharge. This supplement, however, does not prevent or remove eye staining."
 }, {
     "name": "Fur",
-    "img": "../img/products/fur.jpg",
+    "img": "img/products/fur.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/fur",
     "bestseller": false,
     "agility": false,
@@ -132,7 +132,7 @@ var PRODUCTS = [{
     "description": "Encourages fur regrowth and healthy skin."
 }, {
     "name": "Gain",
-    "img": "../img/products/gain.jpg",
+    "img": "img/products/gain.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/gain",
     "bestseller": false,
     "agility": false,
@@ -144,7 +144,7 @@ var PRODUCTS = [{
     "description": "Supports a healthy appetite to promote your pet’s weight gain."
 }, {
     "name": "Happy",
-    "img": "../img/products/happy.jpg",
+    "img": "img/products/happy.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/happy",
     "bestseller": true,
     "agility": false,
@@ -156,7 +156,7 @@ var PRODUCTS = [{
     "description": "Reduces stress from grief, separation anxiety, and environmental changes. It helps your pet feel better and get back to normal."
 }, {
     "name": "Healthy Ear",
-    "img": "../img/products/healthy-ear.jpg",
+    "img": "img/products/healthy-ear.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/healthy-ear",
     "bestseller": false,
     "agility": false,
@@ -168,7 +168,7 @@ var PRODUCTS = [{
     "description": "Helps with your pet’s itchy ears and chronic ear problems."
 }, {
     "name": "Heat",
-    "img": "../img/products/heat.jpg",
+    "img": "img/products/heat.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/heat",
     "bestseller": false,
     "agility": false,
@@ -180,7 +180,7 @@ var PRODUCTS = [{
     "description": "Encourages good circulation and body warmth. Use to improve or maintain healthy circulation in elderly pets."
 }, {
     "name": "Immune",
-    "img": "../img/products/immune.jpg",
+    "img": "img/products/immune.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/immune",
     "bestseller": true,
     "agility": false,
@@ -204,7 +204,7 @@ var PRODUCTS = [{
     "description": "Supports healthy kidney function and the urinary tract in your pets."
 }, {
     "name": "Legs",
-    "img": "../img/products/legs.jpg",
+    "img": "img/products/legs.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/legs",
     "bestseller": false,
     "agility": true,
@@ -216,7 +216,7 @@ var PRODUCTS = [{
     "description": "Used for joint and leg support. It helps your pet get up, turn around and have increased stability and strength in the hips and legs."
 }, {
     "name": "Massage",
-    "img": "../img/products/massage.jpg",
+    "img": "img/products/massage.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/message",
     "bestseller": false,
     "agility": true,
@@ -228,7 +228,7 @@ var PRODUCTS = [{
     "description": "Relaxes cramped or fatigued muscles."
 }, {
     "name": "Power",
-    "img": "../img/products/power.jpg",
+    "img": "img/products/power.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/power",
     "bestseller": false,
     "agility": true,
@@ -240,7 +240,7 @@ var PRODUCTS = [{
     "description": "Promotes strong, mobile joints. This works great with elderly pets or pets with arthritis."
 }, {
     "name": "Relief",
-    "img": "../img/products/relief.jpg",
+    "img": "img/products/relief.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/relief",
     "bestseller": true,
     "agility": true,
@@ -252,7 +252,7 @@ var PRODUCTS = [{
     "description": "Offers muscle and joint pain relief and reduces back pain due to arthritis or injuries."
 }, {
     "name": "Repair",
-    "img": "../img/products/repair.jpg",
+    "img": "img/products/repair.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/repair",
     "bestseller": true,
     "agility": false,
@@ -264,7 +264,7 @@ var PRODUCTS = [{
     "description": "Boosts the body’s immune response.  It is good for injury recovery and supports the body during illness."
 }, {
     "name": "Revive",
-    "img": "../img/products/revive.jpg",
+    "img": "img/products/revive.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/revive",
     "bestseller": false,
     "agility": false,
@@ -276,7 +276,7 @@ var PRODUCTS = [{
     "description": "Increases your pet’s energy and helps them recover from injury or illness. It contains energy boosting multivitamins for dogs and cats."
 }, {
     "name": "Skin",
-    "img": "../img/products/skin.jpg",
+    "img": "img/products/skin.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/skin",
     "bestseller": true,
     "agility": false,
@@ -288,7 +288,7 @@ var PRODUCTS = [{
     "description": "Promotes healthy skin and coats for dogs and cats. It also relieves scratchy, dry skin and helps ease discomfort from flea bites and other skin irritants."
 }, {
     "name": "Sleep",
-    "img": "../img/products/sleep.jpg",
+    "img": "img/products/sleep.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/sleep",
     "bestseller": true,
     "agility": false,
@@ -300,7 +300,7 @@ var PRODUCTS = [{
     "description": "Encourages restful, continuous sleep. This is especially helpful for pets with anxiety or stress."
 }, {
     "name": "Tummy",
-    "img": "../img/products/tummy.jpg",
+    "img": "img/products/tummy.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/tummy",
     "bestseller": true,
     "agility": false,
@@ -312,7 +312,7 @@ var PRODUCTS = [{
     "description": "Relieves your pet’s stomach pain and nausea."
 }, {
     "name": "Vitality",
-    "img": "../img/products/vitality.jpg",
+    "img": "img/products/vitality.jpg",
     "link": "https://epic-pet-health.myshopify.com/collections/frontpage/products/vitality",
     "bestseller": false,
     "agility": false,
